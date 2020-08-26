5dd1c8302a99d8fbe0d0153368b1da71be1b5e3e
= <=>
2020-08-25 21:44:56 -0300

    aula 01

---
+++ projetos/codigo.js
@@ -0,0 +1,87 @@
+// Variáveis e tipos de dados
+
+var nome = 'João'
+
+console.log(nome)
+
+var sobrenome = 'Silva'
+var idade = 28
+
+console.log(sobrenome)
+
+console.log(idade)
+
+var ehMaiorDeIdade = true
+
+console.log(ehMaiorDeIdade)
+
+var cargo
+
+console.log(cargo)
+
+cargo = 'Motorista'
+
+console.log(cargo)
+
+cargo = 'Padeiro'
+
+console.log(cargo)
+
+console.log('O nome é ' + nome)
+
+console.log(nome + ' ' + sobrenome)
+
+console.log('O nome é ' + nome + ' ' + sobrenome)
+
+console.log(nome + ' ' + idade)
+
+console.log('Ele é maior de idade? ' + ehMaiorDeIdade)
+
+// Nomenclatura de variáveis
+
+var NOME = 'Mario'
+
+idade = 30
+
+console.log(idade)
+
+idade = 'trinta e um'
+
+console.log(idade)
+
+var dataDeNascimento
+
+console.log(dataDeNascimento)
+
+dataDeNascimento = '12/10/2000'
+console.log(dataDeNascimento)
+
+dataDeNascimento = '14/10/2000'
+console.log(dataDeNascimento)
+
+dataDeNascimento = null
+console.log(dataDeNascimento)
+
+/// declaração de várias variáveis
+
+var sexo, cor, religiao
+
+sexo = 'Masculino'
+cor = 'Pardo'
+religiao = 'Protestante'
+
+console.log('Sexo: ' + sexo + '. Cor: ' + cor + '. Religião: ' + religiao + '.')
+
+var frase = "Eu vou comer no Bob's"
+
+console.log(frase)
+
+console.log('Ela disse "Olá"')
+
+// alert(nome + ' ' + sobrenome)
+
+nome = prompt('Digite seu nome')
+
+sobrenome = prompt('Digite seu sobrenome')
+
+console.log(nome + ' ' + sobrenome)
